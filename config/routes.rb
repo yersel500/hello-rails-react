6eb87fea49265afe3ceae0dfdb06439df99b5132
yersel500 <yersel500@gmail.com>
2022-07-05 16:08:22 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :v1, defaults: { format: 'json' } do
+    get 'posts', to: 'posts#index'
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '*page', to: 'static#index', constraints: lambda { |req|
+    !req.xhr? && req.format.html?
+  }
+
+  root 'static#index'
 end
